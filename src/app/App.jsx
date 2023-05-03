a2bb95dc250c48053148efa13715c8cc2eded0e3
Nightz <peerapat.ud@gmail.com>
2023-05-03 13:33:40 +0700

    registerpage done

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
 import "./App.scss";
 import TodoPage from "../pages/TodoPage";
 import LoginPage from "../pages/LoginPage";
-import RegisterPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
 import ProfilePage from "../pages/ProfilePage";
 
 function App() {
-  return <LoginPage />;
+  // return <LoginPage />;
   // return <TodoPage />;
+  return <RegisterPage />;
 }
 
 export default App;
